f65de48cab862beb8d84269bf5ed5ed2c04e4776
yoshi huang <yoshihuang.2008@gmail.com>
2026-04-08 10:53:49 +0000

    fix problem

--- frontend/main.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:56536566033b6c0c4cc22b1e8c026916158abd071dc1c7ea84d9c00ee89459d4
-size 11931
--- frontend/ui/worker.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b6aec5d4e98f49eb176c232c11cfbdd20e853daeb70fc99511e3cc6f5baaa291
-size 1991
